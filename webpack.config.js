8372e9982b73c8aab5a3144cacf6af212e12308b
klae32 <techie.r13@gmail.com>
2017-10-05 07:34:14 -0600

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	entry: [
+	'./src/App.js'
+	],
+	output: {
+		path: __dirname, 
+		filename: 'app.js'
+	},
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			loader: 'babel'
+		}]
+	}
+};
+
+// module.exports = {
+// 	entry: [
+// 	'./src/App.js'
+// 	],
+// 	output: {
+// 		path: __dirname, 
+// 		filename: 'app.js'
+// 	},
+// 	module: {
+// 		loaders: [{
+// 			test: /\.jsx?$/,
+// 			exclude: /node_modules/,
+// 			loader: 'babel',
+// 			query: {
+// 				presets: ['es2015', 'react']
+// 			}
+// 		}]
+// 	}
+// };
